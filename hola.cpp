43c152671e15c53032608dc4642ede36fc98f983
Diego Gonzalez <diego.gonzalez@copernic.cat>
2023-11-06 09:06:03 +0100

    test from local 2

--- hola.cpp
+++ hola.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+//https://code.visualstudio.com/docs/cpp/config-linux
+
 using namespace std;
 int main()
 {
    
-    cout << "Hello World Diego github" << endl;
+       vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+
+    int a=0;
+    cout << "\nEscribe un entero para la variable a, actualmente vale: " << a << "\n";
+    cout << " a: ";
+    cin >> a;
+    cout << "a vale ahora: " << a << "\n";
+
+    //test
+
     return 0;
 }
 
